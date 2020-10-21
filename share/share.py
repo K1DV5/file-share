83bdff576020dc4e7e346838e81f865aa9c0d617
K1DV5 <kidusadugna@gmail.com>
2020-10-21 18:37:53 +0300

    add multithreading, partial content, clarify urls

--- share/share.py
+++ share/share.py
@@ -1,11 +1,13 @@
-# -{cd ../../../.. | share}
+# -{cd ../../../../../Installers | share}
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from os import path, listdir, remove
+from os import path, listdir, remove, stat
+from io import DEFAULT_BUFFER_SIZE
 from cgi import FieldStorage
 from json import dumps
 from mimetypes import types_map
 from urllib.parse import unquote
+from socketserver import ThreadingMixIn
 
 index = path.join(path.dirname(__file__), 'index.html')
 own_files = {}  # files here that came from the clients to grant delete access
@@ -13,9 +15,9 @@ own_files = {}  # files here that came from the clients to grant delete access
 
 class handler(BaseHTTPRequestHandler):
 
-    def send_node(self, node):
-        self.send_response(200)
+    def send_node(self, node, hrader_only=False):
         if path.isdir(node):
+            self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
             to_write = []
@@ -30,26 +32,60 @@ class handler(BaseHTTPRequestHandler):
                                  'own': fname in own})
             self.wfile.write(dumps(to_write, ensure_ascii=False).encode())
             return
-        self.send_header('Content-Type', types_map.get(path.splitext(node)[1]))
+        size = stat(node).st_size
         with open(node, 'rb') as file:
-            content = file.read()
-            self.send_header('Content-Length', len(content))
-        self.end_headers()
-        self.wfile.write(content)
+            if 'Range' in self.headers:
+                self.send_response(206)
+                try:
+                    start, end = self.headers.get('Range').strip().split('bytes=')[1].strip().split('-')
+                except IndexError:
+                    self.send_response(416)
+                    return self.end_headers()
+                if start == '':  # last end bytes
+                    to_read = int(end)
+                    file.seek(size - to_read)
+                elif end == '':  # starting from start
+                    start = int(start)
+                    file.seek(start)
+                    to_read = size - start
+                else:
+                    start, end = int(start), int(end)
+                    if start:
+                        file.seek(start)
+                    to_read = end - start
+            else:
+                self.send_response(200)
+                to_read = size
+            self.send_header('Content-Length', to_read)
+            self.send_header('Content-Type', types_map.get(path.splitext(node)[1]))
+            self.end_headers()
+            while to_read > DEFAULT_BUFFER_SIZE:
+                self.wfile.write(file.read(DEFAULT_BUFFER_SIZE))
+                to_read -= DEFAULT_BUFFER_SIZE
+            self.wfile.write(file.read(to_read))
 
     def do_GET(self):
         # for files
-        if self.path == '/':
-            return self.send_node(index)
-        elif self.path.startswith('/get/'):
-            self.path = unquote(self.path[4:]).strip('/') \
-                if self.path[5:] else '.'
+        if self.path == '/' and self.headers.get('Sec-Fetch-Mode') == 'navigate':
+            return self.send_node(index)  # first entry
+        self.path = unquote(self.path).strip('/') if self.path[1:] else '.'
         if path.exists(self.path):
             return self.send_node(self.path)
         self.send_response(404)
         self.send_header('Content-Type', 'text/plain')
         self.end_headers()
         self.wfile.write(b'404: Not found')
+
+    def do_HEAD(self):
+        # for files
+        if self.path == '/' and self.headers.get('Sec-Fetch-Mode') == 'navigate':
+            return self.send_node(index, True)  # first entry
+        self.path = unquote(self.path).strip('/') if self.path[1:] else '.'
+        if path.exists(self.path):
+            return self.send_node(self.path, True)
+        self.send_response(404)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
 
     def do_POST(self):
         self.send_response(200)
@@ -86,6 +122,15 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
 
 
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
+
+
 def serve():
     print('Serving...')
-    HTTPServer(('', 80), handler).serve_forever()
+    server = ThreadingSimpleServer(('', 80), handler)
+    try:
+        while True:
+            server.handle_request()
+    except KeyboardInterrupt:
+        print("\nShutting down server per users request.")
